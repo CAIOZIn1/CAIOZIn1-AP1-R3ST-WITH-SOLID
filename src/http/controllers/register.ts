e1fd5abc271934098bcd122cc11d7322d59f87ab
CAIOZin1 <sellacaio4@gmail.com>
2024-04-21 18:19:51 -0300

    adicionando responsabilidades únicas, distribuição de rotas e hashing de senha

---
+++ src/http/controllers/register.ts
@@ -0,0 +1,21 @@
+import { registerUseCase } from '@/use-cases/register'
+import { FastifyReply, FastifyRequest } from 'fastify'
+import { z } from 'zod'
+
+export async function register(req: FastifyRequest, res: FastifyReply) {
+  const registerBodySchema = z.object({
+    name: z.string(),
+    email: z.string().email(),
+    password: z.string().min(6),
+  })
+
+  const { name, email, password } = registerBodySchema.parse(req.body)
+
+  try {
+    await registerUseCase({ name, email, password })
+  } catch (err) {
+    return res.status(409).send({ error: err })
+  }
+
+  return res.status(201).send()
+}
