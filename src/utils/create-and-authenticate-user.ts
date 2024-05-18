012196e64d374279d233553d968c464476bb16d7
CAIOZin1 <sellacaio4@gmail.com>
2024-05-18 17:30:48 -0300

    :test_tube: test: corrigindo testes com novas permissões do usuário

--- src/utils/create-and-authenticate-user.ts
+++ src/utils/create-and-authenticate-user.ts
@@ -1,7 +1,21 @@
+import { prisma } from '@/lib/prisma'
 import { FastifyInstance } from 'fastify'
 import request from 'supertest'
+import { hashing } from './hashing'
 
-export async function createAndAuthenticateUser(app: FastifyInstance) {
+export async function createAndAuthenticateUser(
+  app: FastifyInstance,
+  isAdmin = false,
+) {
+  const user = await prisma.user.create({
+    data: {
+      name: 'Jonh Doe',
+      email: 'johndoe@example.com',
+      password_hash: await hashing('123456'),
+      role: isAdmin ? 'ADMIN' : 'MEMBER',
+    },
+  })
+
   await request(app.server).post('/users').send({
     name: 'John Doe',
     email: 'johndoe@example.com',
